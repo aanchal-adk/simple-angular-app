7b9e10c2f0ed2e57cfad88f01caa8d757787ee7a
aanchal-adk <aanchal.adk@gmail.com>
2020-08-31 00:17:13 +0545

    4 files modified, 4 added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  items= [];
+
+  constructor() { }
+
+  addToCart (product) {
+    this.items.push(product)
+    
+    return this.items
+  }
+
+  getItems () {
+    return this.items
+  }
+
+  clearCart() {
+    // this.items = []
+
+    return this.items
+  }
+
+}
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service'
 
 @Component({
   selector: 'app-product-details',
@@ -12,9 +13,18 @@ export class ProductDetailsComponent implements OnInit {
 
   product:object;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) { }
+
+  addToCart (product:object) {
+    this.cartService.addToCart(product)
+    window.alert("Sucessfully added to cart!")
+  }
 
   ngOnInit() {
+    console.log(this.cartService)
     this.route.paramMap.subscribe((params) => {
       this.product = products[+params.get('productId')];
     });
